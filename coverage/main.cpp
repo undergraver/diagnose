b7f38737561b4f74a49817c61f08737e4fe8a2b3
Iulian Serbanoiu <iulian-nicu.serbanoiu@mitel.com>
2025-05-31 20:28:04 +0300

    Added main.cpp as an example of coverage

---
+++ coverage/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main() {
+    int x = 5, y = 10;
+    printf("Max is %d\n", max(x, y));
+    return 0;
+}
+
